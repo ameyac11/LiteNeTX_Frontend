c128b704adcd59c4cd38d9d072345ef7e78690d0
ameyac11 <ameyachopade402@gmail.com>
2026-06-28 11:17:49 +0530

    bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
+import clarity from "@microsoft/clarity";
 import App from "./App.tsx";
 import "./index.css";
+
+clarity.init("xdych1gxkq");
 
 createRoot(document.getElementById("root")!).render(<App />);
